f18e978768dc494add5ffdea6cf28587393d8bec
Михаил <@zaymi0>
2024-03-04 15:57:57 +0300

    Updated

--- src/hw32.java
+++ src/hw32.java
@@ -1,5 +1,29 @@
 public class hw32 {
     public static void main(String[] args) {
+        byte num1 = 1;
+        System.out.println(num1);
+
+        short num2 = 2;
+        System.out.println(num2);
+
+        int num3 = 4;
+        System.out.println(num3);
+
+        long num4 = 8;
+        System.out.println(num4);
+
+        float num5 = 4;
+        System.out.println(num5);
+
+        double num6 = 8;
+        System.out.println(num6);
+
+        char num7 = '2';
+        System.out.println(num7);
+
+        boolean num8 = true;
+        System.out.println(num8);
+
 
     }
 }
--- src/hw33.java
+++ src/hw33.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class hw33 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст");
+
+        String str = scanner.nextLine();
+        System.out.println(str);
+        System.out.println("Длина заданной строки: " + str.length());
+
+        String str1 = str + " " + str;
+        System.out.println(str1);
+
+        String str2 = str + " " + 123;
+        System.out.println(str2);
+
+        char ch = str.charAt(0);
+        System.out.println(ch);
+
+
+
 
     }
 }
